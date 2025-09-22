b8c4ffb6e23dd4ce64efff650af167ab7fbab033
Maria <mvalenciacuberos@gmail.com>
2025-09-22 20:57:33 +0200

    Add success message and check how long it took

--- database/db_orchestrator.py
+++ database/db_orchestrator.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import time
 from pathlib import Path
 from pprint import pp
 from load_entries import prepare_entries, load_entries
@@ -21,8 +22,6 @@ def get_parsed_files():
 
     for file in parsed_dir.glob("*.json"):
         parsed_files.append(file.name)
-
-#    pp(parsed_files)
 
     return parsed_files
 
@@ -54,36 +53,13 @@ def get_files_for_processing():
 
     return files_to_process
 
-# files = get_file_for_processing()
-
-# run data prep with found file
-
-# prepare_entries(filename)
-
-# def log_data_prep(db_loading_log=None):
-#     prep_status = prepare_entries(filename)
-
-#     log_entry = [{
-#         "filename": prep_status["filename"],
-#         "processing_done": prep_status["processing_done"],
-#         "loading_done": prep_status["loading_done"],
-#         "entry_count": prep_status["entry_count"],
-#         "people_logged": prep_status["people_logged"],
-#         "timestamp": prep_status["timestamp"]
-#     }]
-
-#     db_loading_log.append(log_entry)
-
-#     with open(log_file, "w") as f:
-#         json.dump(db_loading_log, f, ensure_ascii=False, indent=2)
-#     return db_loading_log
 
 def prep_and_load_files():
-
+    start_time = time.time()
 # 1. get list
     files_for_processing = get_files_for_processing()
-
-# 2. loop through list
+    file_count = 0
+# 2. loop through list, log each part of the process separately
     for filename in files_for_processing:
         prep_status = prepare_entries(filename)
         current_log = read_log()
@@ -112,52 +88,19 @@ def prep_and_load_files():
             with open(log_file, "w") as f:
                 json.dump(current_log, f, ensure_ascii=False, indent=2)
 
-# 2a. for each file in the list: prepare_entries(filename), return status
+        if prep_status["processing_done"] and load_status["loading_done"]:
+            file_count += 1
+    end_time = time.time()
+    duration = end_time - start_time
+    pp(f"{file_count} entries were prepped & loaded successfully in {duration:.2f} seconds 🥳")
 
-# 2b. move logging function in here for prep log
+def main():
+    prep_and_load_files()
 
-
-
-        # log_entry = [{
-        #     "filename": prep_status["filename"],
-        #     "processing_done": prep_status["processing_done"],
-        #     "loading_done": prep_status["loading_done"],
-        #     "entry_count": prep_status["entry_count"],
-        #     "people_logged": prep_status["people_logged"],
-        #     "timestamp": prep_status["timestamp"]
-        # }]
-
-        # db_loading_log.append(log_entry)
-
-# 2c. call load_entries(prepared_entries), return loading_status
-
-# 2d. append loading status to log
-
-# 2e. add a counter to print a success/failure message at the end?
+if __name__ == "__main__":
+    main()
 
 # 3. print an overview of the results to the console (the logging data is saved anyway, I don't need to store that as well)
-
-
-# NOT SURE HOW TO HANDLE THIS:
-# Option 1: once both processing_done & loading_done are TRUE, remove the current filename from the list (or is that not necessary?). Only update json file at the end of the loop.
-    # Pro: One list is worked through sequentially, less json opening/closing/rewriting, the log is created for one entire file processing loop, however many unprocessed files there were found at the beginning of the process. If new files are parsed & saved WHILE this process is running, it doesn't interfere with the list but the orchestrator can be started freshly afterwards.
-
-# Option 2: create the processing list WITHIN the loop for each file?
-    # Pro: would instantly add new files if parser & loading process run parallel.
-    # Con: it feels harder to "supervise", could lead to issues? Doesn't feel as safe?
-
-# 2e. write log for 1 file to json
-
-        # with open(log_file, "w") as f:
-        #     json.dump(db_loading_log, f, ensure_ascii=False, indent=2)
-        # return db_loading_log
-
-
-
-
-
-
-
 
 
 # get_parsed_files()
